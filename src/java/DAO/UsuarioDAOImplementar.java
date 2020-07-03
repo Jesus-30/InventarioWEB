c61fd4cf20bc5db696d682ba179d8c85a3802d86
Estefany_Perez <estefanylinares59@gmail.com>
2020-07-03 16:02:36 -0600

    -Preuba de listarUsuario Funcionando

--- src/java/DAO/UsuarioDAOImplementar.java
+++ src/java/DAO/UsuarioDAOImplementar.java
@@ -1,7 +1,11 @@
 
 package DAO;
 
+import Factory.ConexionDB;
+import Factory.FactoryConexionDB;
 import Model.Usuario;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,11 +13,42 @@ import java.util.List;
  * @author Estefany
  */
 public class UsuarioDAOImplementar implements UsuarioDAO {
+ConexionDB conn;
 
+    public UsuarioDAOImplementar() {
+    }
+    
     @Override
     public List<Usuario> Listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
+        StringBuilder misql = new StringBuilder();
+        misql.append("SELECT * FROM tb_usuario");
+        List<Usuario> lista = new ArrayList<Usuario>();
+        try {
+            ResultSet resultadoSql = this.conn.cosultaSQL(misql.toString());
+            while (resultadoSql.next()) {                
+                Usuario usuario = new Usuario();
+                usuario.setId(resultadoSql.getInt("id"));
+                usuario.setNombre(resultadoSql.getString("nombre"));
+                usuario.setApellido(resultadoSql.getString("apellido"));
+                usuario.setCorreo(resultadoSql.getString("correo"));
+                usuario.setUsuario(resultadoSql.getString("usuario"));
+                usuario.setClave(resultadoSql.getString("clave"));
+                usuario.setTipo(resultadoSql.getInt("tipo"));
+                usuario.setEstado(resultadoSql.getInt("estado"));
+                usuario.setPregunta(resultadoSql.getString("pregunta"));
+                usuario.setRepuesta(resultadoSql.getString("respuesta"));
+                usuario.setFecha_registro(resultadoSql.getDate("fecha_registro"));
+                lista.add(usuario);
+            }
+        } catch (Exception ex) {
+           
+        } finally {
+            this.conn.cerrarConexion();
+        }
+        return lista;
     }
+
 
     @Override
     public List<Usuario> Listar2() {
--- src/java/Test/PruebaUsuario.java
+++ src/java/Test/PruebaUsuario.java
@@ -1,10 +1,36 @@
 
 package Test;
 
+import DAO.UsuarioDAO;
+import DAO.UsuarioDAOImplementar;
+import Model.Usuario;
+import java.util.List;
+
 /**
  *
  * @author Estefany
  */
 public class PruebaUsuario {
-    
+     public static void main(String[] args) {
+    PruebaUsuario evaluar = new PruebaUsuario();
+ evaluar.listarUsuario();
+}
+     public void listarUsuario(){
+        UsuarioDAO usuario = new UsuarioDAOImplementar();
+        List<Usuario> listar = usuario.Listar();
+        System.out.println("LISTADO DE USUARIOS");
+        for(Usuario usuarioListar : listar){
+            System.out.println("ID: " + usuarioListar.getId() + 
+                    " \nNombre: " + usuarioListar.getNombre() + 
+                    " \nApellido: " + usuarioListar.getApellido() + 
+                    " \nCorreo: " + usuarioListar.getCorreo() + 
+                    " \nUsuario: " + usuarioListar.getUsuario() +
+                    " \nClave: " + usuarioListar.getClave() +
+                    " \nTipo: " + usuarioListar.getTipo() + 
+                    " \nEstado: " + usuarioListar.getEstado() + 
+                    " \nPregunta: " + usuarioListar.getPregunta() +
+                    " \nRespuesta: " + usuarioListar.getRepuesta() +
+                    " \nFecha: " + usuarioListar.getFecha_registro());
+        }
+    }
 }
