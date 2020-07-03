14d595a5ff9faf1120e414c3596b2214a2dd02cb
Estefany_Perez <estefanylinares59@gmail.com>
2020-07-03 15:29:37 -0600

    -Se creo el constructor Productos -Vista listarProducto muestra la lista de los productos

--- src/java/Controller/Productos.java
+++ src/java/Controller/Productos.java
@@ -1,12 +1,17 @@
 
 package Controller;
 
+import DAO.ProductoDAO;
+import DAO.ProductoDAOImplementar;
+import Model.Producto;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -14,6 +19,20 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Productos extends HttpServlet {
 
+    protected void listarProductos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        ProductoDAO producto = new ProductoDAOImplementar();
+        HttpSession session = request.getSession(true);
+        session.setAttribute("listas", producto.Listar());
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Productos/listarProducto.jsp");
+        dispatcher.forward(request, response);
+    }
+    
+    protected void borrarProducto(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Vistas-Productos/listarProducto.jsp");
+        dispatcher.forward(request, response);
+    }
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -21,41 +40,71 @@ public class Productos extends HttpServlet {
         
         }
   
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         String parametro = request.getParameter("opcion");
+        if (parametro.equals("crear")) {
+            //action
+            String pagina = "/Vistas-Productos/crearProducto.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+            dispatcher.forward(request, response);
+            
+        } else if(parametro.equals("listar")){
+            this.listarProductos(request, response);
+            
+        } else if(parametro.equals("modificar")){
+            //action
+            int id = Integer.parseInt(request.getParameter("id_prod"));
+            String nombre = request.getParameter("nombre_prod");
+            Double stock = Double.parseDouble(request.getParameter("stock_prod"));
+            Double precio = Double.parseDouble(request.getParameter("precio_prod"));
+            String unidad = request.getParameter("unidad_med");
+            int estado = Integer.parseInt(request.getParameter("estado_prod"));
+            String categoria = request.getParameter("categoria_prod");
+            String descripcion = request.getParameter("desc_prod");
+            
+            String pagina = "/Vistas-Productos/crearProducto.jsp?id_prod="+id+"&&nombre_prod="+nombre+"&&stock_prod="+stock+"&&precio_prod="+precio+"&&unidad_med="+unidad+"&&estado_prod="+estado+"&&categoria_prod="+categoria+"&&desc_prod="+descripcion+"&&RadioV=1";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+            dispatcher.forward(request, response);
+            
+            
+        } else if(parametro.equals("eliminar")){
+            int eliminar_id = Integer.parseInt(request.getParameter("id"));
+            ProductoDAO producto = new ProductoDAOImplementar();
+            producto.borrarPro(eliminar_id);
+            this.listarProductos(request, response);
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        Producto producto = new Producto();
+        int id_product = Integer.parseInt(request.getParameter("id_producto"));
+        String nombre = request.getParameter("txtNombre");
+        double stock = Double.parseDouble(request.getParameter("txtStock"));
+        double precio = Double.parseDouble(request.getParameter("txtPrecio"));
+        String unidadMedida = request.getParameter("txtUnidaMedida");
+        int estado = Integer.parseInt(request.getParameter("txtEstado"));
+        String categoria = request.getParameter("txtCategoria");
+        String desProducto = request.getParameter("txtDescripcion");
+        
+        producto.setId_producto(id_product);
+        producto.setNom_producto(nombre);
+        producto.setStock(stock);
+        producto.setPrecio(precio);
+        producto.setUnidadMedida(unidadMedida);
+        producto.setEstado(estado);
+        producto.setCategoria_id(categoria);
+        producto.setDes_producto(desProducto);
+        
+        ProductoDAO productoG = new ProductoDAOImplementar();
+        productoG.guardarPro(producto);
+        this.listarProductos(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
--- web/Vistas-Productos/listarProducto.jsp
+++ web/Vistas-Productos/listarProducto.jsp
@@ -1,12 +1,65 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+<%@page import="Model.Producto"%> 
+<jsp:useBean id= "listas" scope="session" class="java.util.List"/>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+       <title>CONTROL DE INVENTARIO</title>
+        <%@include file="../WEB-INF/Vistas-Parciales/css-js.jspf" %>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <%@include file="../WEB-INF/Vistas-Parciales/encabezado.jspf" %>
+            <h3>Listado de Productos</h3>
+       <div id="cuadrolistar0" class="shadow-lg p-3 mb-5 bg-white rounded">
+            <table class="table table-striped">
+                <tr>
+             
+                    <th>ID</th>
+                    <th>NOMBRE PRODUCTO</th>
+                    <th>STOCK</th>
+                    <th>PRECIO</th>
+                    <th><center>UNIDAD MEDIDA</center></th>
+                    <th>ESTADO PRODUCTO</th>
+                    <th>CATEGORIAS</th>
+                    <th><center>DESCRIPCION PRODUCTO</center></th>
+                    <th>FECHA DE ENTRADA</th>
+                    <th><center>ACCIONES</center></th>
+              
+                   
+                </tr>
+                 
+                <%
+                    for(int i = 0; i < listas.size(); i ++){
+                        Producto producto = new Producto ();
+                        producto = (Producto)listas.get(i);
+                    
+                %>
+                <tr>
+                    <td><%=producto.getId_producto() %></td>
+                    <td><%=producto.getNom_producto() %></td>
+                    <td><%= producto.getStock() %></td>
+                    <td><%= producto.getPrecio() %></td>
+                    <td><center><%= producto.getUnidadMedida() %></center></td>
+                    <td><center><%= producto.getEstado() %></center></td>
+            <td><center><%= producto.getCategoria_id() %></center></td>
+            <td><center><%= producto.getDes_producto() %></center></td> 
+            <td><%=producto.getFecha_entrada() %></td>
+                    <td >
+  <a  href="<%= request.getContextPath() %>/productos?opcion=modificar&&id_prod=<%= producto.getId_producto() %>&&nombre_prod=<%= producto.getNom_producto() %>&&stock_prod=<%= producto.getStock() %>&&precio_prod=<%= producto.getPrecio() %>&&unidad_med=<%= producto.getUnidadMedida() %>&&estado_prod=<%= producto.getEstado() %>&&categoria_prod=<%= producto.getCategoria_id() %>&&desc_prod=<%= producto.getDes_producto() %>" class="btn btn-info btn-sm glyphicon glyphicon-edit" role="button" name="btnmodi">Editar</a>
+  <a  href="<%= request.getContextPath() %>/productos?opcion=eliminar&&id=<%= producto.getId_producto() %>" class="btn btn-danger btn-sm glyphicon glyphicon-remove" role="button" >Borrar</a>
+                    </td>
+      
+                </tr>
+                
+                <%
+                    }
+                %>
+            </table>
+           <a id="button" href="<%= request.getContextPath() %>/productos?opcion=crear" class="btn btn-success" role="button"> Nuevo Producto</a>
+            <a id="pdf" href="crearPDF" class="btn btn-warning">Imprimir PDF</a>
+        </div>
     </body>
+     <%@include file="../WEB-INF/Vistas-Parciales/pie.jspf" %>
 </html>
