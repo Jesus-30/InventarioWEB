5ccc807a08163ad4fac29ad7758092eba262e8a2
Estefany_Perez <estefanylinares59@gmail.com>
2020-07-03 14:35:03 -0600

    -Se creo la clase ProductoDAO que es una clase interface se definieron los metodos . -Se creo otra clase ProductoDAOImplementar y se implementaron los métodos Abstractos de la clase ProductoDAO

---
+++ src/java/DAO/ProductoDAOImplementar.java
@@ -0,0 +1,38 @@
+
+package DAO;
+
+import Model.Producto;
+import java.util.List;
+
+/**
+ *
+ * @author Estefany
+ */
+public class ProductoDAOImplementar implements ProductoDAO  {
+
+    @Override
+    public List<Producto> Listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Producto> Listar2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Producto editarPro(int id_pro_edit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean guardarPro(Producto producto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean borrarPro(int id_pro_borrar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
