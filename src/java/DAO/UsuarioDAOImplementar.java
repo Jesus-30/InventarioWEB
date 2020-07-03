023591e6e8a5a792b7fae9a93e0930f8e1ec467b
Estefany_Perez <estefanylinares59@gmail.com>
2020-07-03 16:38:25 -0600

    -Se agregaron las demas funciones para la prueba

--- src/java/DAO/UsuarioDAOImplementar.java
+++ src/java/DAO/UsuarioDAOImplementar.java
@@ -5,6 +5,7 @@ import Factory.ConexionDB;
 import Factory.FactoryConexionDB;
 import Model.Usuario;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,13 +58,73 @@ ConexionDB conn;
 
     @Override
     public Usuario editarUsuario(int id_U_edit) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
+       this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
+        Usuario usuario = new Usuario();
+        StringBuilder miSQL = new StringBuilder();
+        
+        miSQL.append("SELECT * FROM tb_usuario where id =").append(id_U_edit);
+        try {
+            ResultSet resultadoSql = this.conn.cosultaSQL(miSQL.toString());
+            while (resultadoSql.next()) {                             
+                usuario.setId(resultadoSql.getInt("id"));
+                usuario.setNombre(resultadoSql.getString("nombre"));
+                usuario.setApellido(resultadoSql.getString("apellido"));
+                usuario.setCorreo(resultadoSql.getString("correo"));
+                usuario.setUsuario(resultadoSql.getString("usuario"));
+                usuario.setClave(resultadoSql.getString("clave"));
+                usuario.setTipo(resultadoSql.getInt("tipo"));
+                usuario.setEstado(resultadoSql.getInt("estado"));
+                usuario.setPregunta(resultadoSql.getString("pregunta"));
+                usuario.setRepuesta(resultadoSql.getString("respuesta"));
+                usuario.setFecha_registro(resultadoSql.getDate("fecha_registro"));
+            }
+        } catch (SQLException ex) {
+          
+        } finally {
+            this.conn.cerrarConexion();
+        }
+        return usuario;
     }
 
     @Override
     public boolean guardarUsuario(Usuario usuario) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         this.conn = FactoryConexionDB.open(FactoryConexionDB.MySQL);
+        boolean guardar = false;
+        try {
+            if (usuario.getId() == 0) {
+                StringBuilder miSQL = new StringBuilder();
+                miSQL.append("INSERT INTO tb_usuario(nombre, apellido, correo, usuario, clave, tipo, estado, pregunta, respuesta) values('");
+                miSQL.append(usuario.getNombre() +  "', '").append(usuario.getApellido() + "', '").append(usuario.getCorreo() +  "', '").append(usuario.getUsuario() + 
+                "', '").append(usuario.getClave() + "', '").append(usuario.getTipo() +"', '").append(usuario.getEstado() + "', '").append(usuario.getPregunta()+
+                "', '").append(usuario.getRepuesta());
+                miSQL.append("');");
+                this.conn.ejecutarSQL(miSQL.toString());
+            } else if(usuario.getId() > 0){
+                StringBuilder miSQL = new StringBuilder();
+                miSQL.append("update tb_usuario set id ='").append(usuario.getId());
+                miSQL.append("', nombre = '").append(usuario.getNombre());
+                miSQL.append("', apellido = '").append(usuario.getApellido());
+                miSQL.append("', correo = '").append(usuario.getCorreo());
+                miSQL.append("', usuario = '").append(usuario.getUsuario());
+                miSQL.append("', clave = '").append(usuario.getClave());
+                miSQL.append("', tipo = '").append(usuario.getTipo());
+                miSQL.append("', estado = '").append(usuario.getEstado());
+                miSQL.append("', pregunta = '").append(usuario.getPregunta());
+                miSQL.append("', respuesta = '").append(usuario.getRepuesta());
+                miSQL.append("' where id = ").append(usuario.getId()).append(";");
+                this.conn.ejecutarSQL(miSQL.toString());
+            }
+            guardar = true;
+        } catch (Exception ex) {
+           
+        } finally {
+            this.conn.cerrarConexion();
+        }
+        return guardar;
     }
+
+
 
     @Override
     public boolean borrarUsuario(int id_U_borrar) {
--- src/java/Test/PruebaUsuario.java
+++ src/java/Test/PruebaUsuario.java
@@ -14,6 +14,9 @@ public class PruebaUsuario {
      public static void main(String[] args) {
     PruebaUsuario evaluar = new PruebaUsuario();
  evaluar.listarUsuario();
+// evaluar.editarUsuario();
+ //evaluar.guardarUsu();
+ //evaluar.borrarUsuario();
 }
      public void listarUsuario(){
         UsuarioDAO usuario = new UsuarioDAOImplementar();
@@ -33,4 +36,44 @@ public class PruebaUsuario {
                     " \nFecha: " + usuarioListar.getFecha_registro());
         }
     }
+      public void editarUsuario(){
+        UsuarioDAO usuario = new UsuarioDAOImplementar();
+        Usuario u_edit = usuario.editarUsuario(1);
+        System.out.println("Usuario a Editar");
+        System.out.println("ID: " + u_edit.getId()+ 
+                    " \nNombre: " + u_edit.getNombre() + 
+                    " \nApellido: " + u_edit.getApellido() + 
+                    " \nCorreo: " + u_edit.getCorreo() + 
+                    " \nUsuario: " + u_edit.getUsuario() +
+                    " \nClave: " + u_edit.getClave() +
+                    " \nTipo: " + u_edit.getTipo() + 
+                    " \nEstado: " + u_edit.getEstado() + 
+                    " \nPregunta: " + u_edit.getPregunta() +
+                    " \nRespuesta: " + u_edit.getRepuesta() +
+                    " \nFecha: " + u_edit.getFecha_registro());
+    }
+    public void guardarUsu(){
+         UsuarioDAO usuario = new UsuarioDAOImplementar();
+        Usuario guarda_u = new Usuario();
+        guarda_u.setId(2);
+        guarda_u.setNombre("Jesus");
+        guarda_u.setApellido("Henriquez");
+        guarda_u.setCorreo("j20@gmail.com");
+        guarda_u.setUsuario("administrador");
+        guarda_u.setClave("1502");
+        guarda_u.setTipo(1);
+        guarda_u.setEstado(1);
+        guarda_u.setPregunta("Color favorito");
+        guarda_u.setRepuesta("Amarillo");
+        
+        usuario.guardarUsuario(guarda_u);
+    }
+    public void borrarUsuario(){
+        UsuarioDAO usuario = new UsuarioDAOImplementar();
+        usuario.borrarUsuario(2);
+    }
 }
+
+
+
+
